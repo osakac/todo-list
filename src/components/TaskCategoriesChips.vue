42cdd20ef81a16828b2989a6142eb22ce9512f85
osakac <shinmoriof@gmail.com>
2023-02-27 23:24:42 +0400

    first commit

---
+++ src/components/TaskCategoriesChips.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TaskCategoriesChips"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UI/TaskStatus.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TaskStatus"
+}
+</script>
+
+<style scoped>
+
+</style>
